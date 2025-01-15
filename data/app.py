de34ae73a0096ceb51b44a570b8c12d926c273c4
Hawawou <hawawout2000@gmail.com>
2025-01-15 13:57:03 +0100

    scraped reddit data

--- data/app.py
+++
@@ -1,9 +0,0 @@
-import json
-import requests
-from bs4 import BeautifulSoup
-
-reddit_url = "https://www.reddit.com/r/datascience/"
-response = requests.get(reddit_url)
-soup = BeautifulSoup(response.text, 'html.parser')
-
-articles = soup.find_all('div', class_='_1poyrkZ7g36PawDueRza-J')
